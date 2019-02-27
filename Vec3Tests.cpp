53a1e0460232a8a76d6654a105f6921d12cf22d9
kgeorgiou <georgiou.kwn@gmail.com>
2019-02-27 13:24:32 +0000

    added default ctor

--- Vec3Tests.cpp
+++ Vec3Tests.cpp
@@ -11,3 +11,12 @@ TEST(Vec3, testDefaultConstructor)
     ASSERT_FLOAT_EQ(a.m_y,0.0f);
     ASSERT_FLOAT_EQ(a.m_z,0.0f);
 }
+
+
+TEST(Vec3, userDefinedConstructor)
+{
+    Vec3 a(1.0f,2.0f,3.0f);
+    ASSERT_FLOAT_EQ(a.m_x,1.0f);
+    ASSERT_FLOAT_EQ(a.m_y,2.0f);
+    ASSERT_FLOAT_EQ(a.m_z,3.0f);
+}
